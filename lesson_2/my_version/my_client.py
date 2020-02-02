a69c567fecd710b9a4449d57d987cefd7bf62ca8
SergeyMakhotkin <ser9box@mail.ru>
2020-02-02 14:56:57 +0300

    декораторы логирования

--- lesson_2/my_version/my_client.py
+++ lesson_2/my_version/my_client.py
@@ -1,9 +1,8 @@
 """Клиентская часть"""
 
-from sys import exit
+from sys import argv, exit
 from json import JSONDecodeError
 import socket
-import argparse
 import time
 import logging
 import log_config.client_log_config
@@ -59,46 +58,35 @@ def main():
     """
 
     '''настройка получения аргументов из командной строки'''
-    # try:
-    #     serv_addr = argv[1]
-    #     serv_port = int(argv[2])
-    #     if not (1024 <= serv_port <= 65535):
-    #         CLIENT_LOG.critical(f'Попытка запуска клиента с неподходящим номером порта: {serv_port}.'
-    #                             f' Допустимы адреса с 1024 до 65535. Програбба закрывается.')
-    #         raise ValueError
-    # except IndexError:
-    #     serv_addr = DEFAULT_IP_ADDRESS
-    #     serv_port = DEFAULT_PORT
-    #     CLIENT_LOG.info(f'Запущен клиент с парамертами: '
-    #                     f'адрес сервера: {serv_addr} , порт: {serv_port}')
-    # except ValueError:
-    #     print("Вторым параметром должен быть указан порт (число в диапазоне 1024 - 65535")
-    #     exit(1)
-
-    parser = argparse.ArgumentParser(description="Параметры запуска клиентского скрипта")
-    parser.add_argument('ip', default=DEFAULT_IP_ADDRESS, nargs='?', help='server ip address')
-    parser.add_argument('port', default=DEFAULT_PORT, nargs='?', type=int, help='server tcp port')
-    args = parser.parse_args()
-    if not 1024 <= args.port <= 65535:
-        CLIENT_LOG.critical(f'Попытка запуска клиента с неподходящим номером порта: {args.port}.'
-                            f' Допустимы адреса с 1024 до 65535. Програбба закрывается.')
-        exit()
-
-    CLIENT_LOG.info(f'Программа запущена с параметрами server_ip: {args.ip}, server_port: {args.port}')
+    try:
+        serv_addr = argv[1]
+        serv_port = int(argv[2])
+        if not (1024 <= serv_port <= 65535):
+            CLIENT_LOG.critical(f'Попытка запуска клиента с неподходящим номером порта: {serv_port}.'
+                                f' Допустимы адреса с 1024 до 65535. Програбба закрывается.')
+            raise ValueError
+    except IndexError:
+        serv_addr = DEFAULT_IP_ADDRESS
+        serv_port = DEFAULT_PORT
+        CLIENT_LOG.info(f'Запущен клиент с парамертами: '
+                        f'адрес сервера: {serv_addr} , порт: {serv_port}')
+    except ValueError:
+        print("Вторым параметром должен быть указан порт (число в диапазоне 1024 - 65535")
+        exit(1)
 
     # Создаем сокет, обмениваемся сообщениями с сервером
 
     client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_sock.connect((args.ip, args.port))
+    client_sock.connect((serv_addr, serv_port))
     message = generate_presence_msg()
     send_message(client_sock, message)
     try:
         answer = process_server_ans(get_message(client_sock))
         CLIENT_LOG.info(f'Принят ответ от сервера {answer}')
         client_sock.close()
-        CLIENT_LOG.debug(f'Соединение с сервером {args.ip}:{args.port} закрыто')
+        CLIENT_LOG.debug(f'Соединение с сервером {serv_addr}:{serv_port} закрыто')
     except ConnectionRefusedError:
-        CLIENT_LOG.debug(f'Не удалось подключиться к серверу {args.ip}:{args.port}, '
+        CLIENT_LOG.debug(f'Не удалось подключиться к серверу {serv_addr}:{serv_port}, '
                          f'Соединение сброшено сервером.')
     except (ValueError, JSONDecodeError):
         CLIENT_LOG.error('Не удалось декодировать полученную Json строку.')
